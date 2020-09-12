1861a431cf40b42f226136279bd4402ddb562db7
hanlinhe <hanlin.he@igg>
2020-09-12 15:15:34 +0800

    b'\xe5\x89\x91\xe9\xbe\x99\xe5\x9c\xa8\xe9\xa9\xaf\xe6\x9c\x8d\r\n'

---
+++ 提交.py
@@ -0,0 +1,42 @@
+import os
+import sys
+import platform
+# 添加需要安装的扩展包名称进去
+os.system("pip install GitPython")
+os.system("python -m pip install --upgrade pip")
+
+
+import git
+from git import *
+
+
+def read_file(file):
+        """
+        read .md or .txt format file
+        :param file: .md or .txt format file
+        :return: data
+        """
+        with open(file,'rb') as f:
+            lines = f.readlines()
+        data = []
+        for line in lines:            
+            data.append(line)
+        return data
+
+
+
+
+
+
+
+data = read_file('todo.md')
+
+curFileList = os.path.split(os.path.realpath(__file__))
+repo = Repo(curFileList[0])
+remote = repo.remote()
+remote.pull()
+
+git = repo.git
+git.add('.') 
+git.commit('-m', data[0])
+
