a54309a92cd325d49b297753200ea95bc1ce96d6
hanlinhe <hanlin.he@igg>
2020-09-24 20:32:36 +0800

    本次游戏时间 00时00分11秒 总游戏时间 00时00分41秒目前概况
    - [ ] 采茅草，准备木头，石头，出海挖坑，挖油，搞水泥
    
    - [ ] 带羽暴龙升级
    
    - [ ] 注意迅猛龙
    
    - [ ] 驯服 棘背龙 魔鬼蛙 猛犸
    
    - [ ]
    
    驯服顺序
    
    蛙 鳄
    
    鹰  狼 风神
    
    鱼龙  **沧龙** **蛇颈龙** **龙王鲸**

--- 提交.py
+++ 提交.py
@@ -28,7 +28,7 @@ def read_file(file):
 
 
 
-data = read_file('todo.md')
+mddata = read_file('todo.md')
 
 curFileList = os.path.split(os.path.realpath(__file__))
 repo = Repo(curFileList[0])
@@ -36,10 +36,52 @@ remote = repo.remote()
 remote.pull()
 
 git = repo.git
-git.add('.') 
 
+
+git.add('.') 
 logStr = ""
-for log in data:
+
+import time
+ticks = time.time()
+localtime = time.asctime( time.localtime(time.time()) )
+import json
+# data = {
+#     "启动时间戳":0,
+#     "保存时间戳":0,
+#     "启动时间":0,
+#     "保存时间":0,
+#     "本次运行时长":0,
+#     "总游戏时长":0,
+# }
+
+data={}
+ 
+# 读取数据
+with open(curFileList[0] + '\data.json', 'r',encoding = 'utf8') as f:
+    data = json.load(f)
+
+data["保存时间戳"] = ticks
+data["保存时间"] = localtime
+data["本次运行时长"] = data["保存时间戳"] - data["启动时间戳"]
+data["总游戏时长"] = data["总游戏时长"] + data["本次运行时长"]
+
+def secondsToGameStr(sec):
+    m, s = divmod(sec, 60)
+    h, m = divmod(m, 60)    
+    return "%02d时%02d分%02d秒" % (h, m, s)
+
+
+data["本次游戏时间"] = secondsToGameStr(data["本次运行时长"])
+data["总游戏时间"] = secondsToGameStr(data["总游戏时长"])
+
+# 写入 JSON 数据
+with open(curFileList[0] + '\data.json', 'w',encoding = 'utf8') as f:
+    json.dump(data, f)
+
+
+logStr = "本次游戏时间 " + data["本次游戏时间"] + " 总游戏时间 " +  data["总游戏时间"]
+
+for log in mddata:
     logStr = logStr + log
 git.commit('-m', logStr)
 git.push()
--- 运行游戏.py
+++ 运行游戏.py
@@ -17,13 +17,43 @@ repo = Repo(curFileList[0])
 remote = repo.remote()
 remote.pull()
 
+import time
+ticks = time.time()
+localtime = time.asctime( time.localtime(time.time()) )
 
+import json
+data={}
+
+# data = {
+#     "启动时间戳":0,
+#     "保存时间戳":0,
+#     "启动时间":0,
+#     "保存时间":0,
+#     "本次运行时长":0,
+#     "总游戏时长":0,
+#     "本次游戏时间":0,
+#     "总游戏时间":0,
+# }
+
+ 
+# 读取数据
+with open(curFileList[0] + '\data.json', 'r',encoding = 'utf8') as f:
+    data = json.load(f)
+
+data["启动时间戳"] = ticks
+data["启动时间"] = localtime
 
 import subprocess  
 import os  
 main = "D:\\Games\\Epic Games\\ARKSurvivalEvolved\\ShooterGame\\Binaries\\Win64\\ShooterGame.exe"
 if os.path.exists(main):  
-    rc,out= subprocess.getstatusoutput(main)  
-    print (rc)
-    print ('*'*10)
-    print (out)
+    print("开始运行游戏")
+#    rc,out= subprocess.getstatusoutput(main)  
+    #print (rc)    
+    #print (out)
+    
+
+# 写入 JSON 数据
+with open(curFileList[0] + '\data.json', 'w',encoding = 'utf8') as f:
+    json.dump(data, f)
+
